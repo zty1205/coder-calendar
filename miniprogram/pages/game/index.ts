aab56b94c4e0d2e2c9aad079cc82be9a169a357d
zty1205 <zty1160051490@163.com>
2023-12-20 22:01:33 +0800

    fix: 移除审核失败-扫雷代码

--- miniprogram/pages/game/index.ts
+++ miniprogram/pages/game/index.ts
@@ -13,9 +13,10 @@ Page({
     });
   },
   handleBoom() {
-    wx.navigateTo({
-      url: '/pages_ability/boom/index'
-    });
+    wx.showToast({
+      title: '微信审核失败，敬请期待',
+      icon: 'none',
+    })
   },
   handleSoon() {
     comingSoon();
